3fcc61db8580ed41bd691d1b3c948be05261f37a
Paul Smith <paul@smithops.net>
2019-09-27 00:54:26 -0400

    improved ingest, beta interactive dashboard

--- vp_dash.py
+++ vp_dash.py
@@ -2,9 +2,11 @@ import sys,os,time,datetime,pickle
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import logging
 from chart_studio.plotly import plot_mpl
 from statsmodels.tsa.seasonal import seasonal_decompose
+from datetime import datetime as dt
 
 logging.basicConfig()
 logger = logging.getLogger('logger')
@@ -37,6 +39,7 @@ type_data_sorted = list(type_data[i] for i in to_sort)
 type_names_sorted = list(type_names[i] for i in to_sort)
 
 
+
 pv_raw = np.zeros((pv_array.shape[0],n_days),dtype=np.int64)
 for index,column in enumerate(list(str(i) for i in range(n_days))):
     pv_raw[:,index] = pv_array[column]
@@ -49,21 +52,95 @@ result = seasonal_decompose(pdf, model='multiplicative')
 trend =  result.trend[0].tolist()
 resid = result.resid[0].tolist()
 seasonal = result.seasonal[0].tolist()
+
+def get_dates_index(start_dt,end_dt):
+    ilist = []
+    for index,dt1 in enumerate(date_dt):
+        if dt1 <= start_dt and dt1 >= end_dt:
+            ilist.append(index)
+    return ilist
+def get_date_strings(ilist):
+    dlist = list(date_str for i,date_str in enumerate(date_strings) if i in ilist)
+    return dlist
+
+def get_y_data(data,ilist):
+    data = list(data)
+    if len(data) == 1:
+        data = data[0]
+    dlist = list(datum for i,datum in enumerate(data) if i in ilist)
+    return dlist
+
+
+
+start_dt = start_datetime
+end_dt = end_datetime
+plot1_xdata = date_strings
+plot1_ydata = total_visits
+
+
+#DASHBOARD
+app.layout = html.Div([#L1
+    html.Div([#L2
+        html.Div([ #L3
+            html.H5('Start Date'),
+            dcc.DatePickerSingle(
+                id='my-date-picker-single1',
+                min_date_allowed=date_dt[0],
+                max_date_allowed=date_dt[-1],
+                initial_visible_month=dt(2018, 1, 1),
+                date=str(dt(2018, 1, 1, 00, 00, 00))
+            ),
+            html.Div(id='output-container-date-picker-single1')
+
+        ], className="three columns",style={'width':'25%'}),
+        html.Div([ #L3
+            html.H5('End Date'),
+            dcc.DatePickerSingle(
+                id='my-date-picker-single2',
+                min_date_allowed=dt(2018, 1, 1),
+                max_date_allowed=dt(2018, 4, 1),
+                initial_visible_month=dt(2018, 4, 1),
+                date=str(dt(2018, 4, 1, 00, 00, 00))
+            ),
+            html.Div(id='output-container-date-picker-single2')
+
+        ], className="three columns",style={'width':'25%'}),
+        html.Div([ #L3
+            html.H5('Category'),
+            dcc.Dropdown(
+                id='cat-dropdown',
+                options = list({"label":cat,"value":cat} for cat in cat_names),
+                value=cat_names[3]
+            ),
+            html.Div(id="cat_select_output", style={'display':'none'})
+
+        ], className="three columns",style={'width':'25%'}),
+        html.Div([html.Button('Model', id='model_button')]),
+    ],className="row"),
+    html.Div([dcc.Graph(id='plot1')]),
+    html.Div([dcc.Graph(id='plot2')]),
+],
+)
+
+
+"""
 app.layout = html.Div(children=[
     html.H1(children='Vinepair Pageviews'),
-    html.Div(children='Total Views'),
-    dcc.Graph(
-        id='pageviews',
-        figure={
-            'data': [
-                {'x': date_strings, 'y': total_visits, 'type': 'bar', 'name': 'Max Views'},
-                {'x': date_strings, 'y': total_visits, 'type': 'line', 'name': 'Max Views'},
-            ],
-            'layout': {
-                'title': 'Total Pageviews vs. Date'
+    html.Div([
+#        children='Total Views',
+        dcc.Graph(
+            id='pageviews',
+            figure={
+                'data': [
+                    {'x': date_strings, 'y': total_visits, 'type': 'bar', 'name': 'Max Views'},
+                    {'x': date_strings, 'y': total_visits, 'type': 'line', 'name': 'Max Views'},
+                ],
+                'layout': {
+                    'title': 'Total Pageviews vs. Date'
+                }
             }
-        }
-    ),
+        )
+    ],
     html.Div(children='Categories'),
     dcc.Graph(
         id='Categories',
@@ -144,5 +221,57 @@ app.layout = html.Div(children=[
     
 ])
 
+@app.callback(
+    Output('output-container-date-picker-single1', 'children'),[Input('my-date-picker-single1', 'date')])
+def update_start_date(date):
+    if date is not None:
+        date = dt.strptime(date.split(' ')[0], '%Y-%m-%d')
+        update_plot1(plot1_ydata,date,end_dt)
+        date_string = date.strftime('%B %d, %Y')
+        return date_string
+@app.callback(
+    Output('output-container-date-picker-single2', 'children'),[Input('my-date-picker-single2', 'date')])
+def update_end_date(date):
+    if date is not None:
+        date = dt.strptime(date.split(' ')[0], '%Y-%m-%d')
+        date_string = date.strftime('%B %d, %Y')
+        return date_string
+"""
+@app.callback(
+    Output('plot1', 'figure'),[Input('cat-dropdown', 'value'),
+                               Input('my-date-picker-single2', 'date'),
+                               Input('my-date-picker-single1','date')])
+def update_plot1(cat,start_date,end_date):
+    start_dt = dt.strptime(start_date.split(' ')[0], '%Y-%m-%d')
+    end_dt = dt.strptime(end_date.split(' ')[0], '%Y-%m-%d')
+    ilist = get_dates_index(start_dt,end_dt)
+    plot1_xdata = get_date_strings(ilist)
+    data = list(cat_array[np.nonzero(cat_array['category'] == cat)[0]][list(str(i) for i in ilist)])
+    plot1_ydata = get_y_data(data,ilist)
+    return {'data': [{'x': plot1_xdata, 'y': plot1_ydata, 'type': 'bar', 'name': 'PageViews'},],
+                'layout': {'title': '%s vs. Date' % cat.upper()}}
+@app.callback(
+    Output('plot2', 'figure'),[Input('cat-dropdown', 'value'),
+                               Input('my-date-picker-single2', 'date'),
+                               Input('my-date-picker-single1','date'),
+                               Input('model_button', 'n_clicks')])
+def model_data(cat,start_date,end_date,n_clicks):
+    start_dt = dt.strptime(start_date.split(' ')[0], '%Y-%m-%d')
+    end_dt = dt.strptime(end_date.split(' ')[0], '%Y-%m-%d')
+    ilist = get_dates_index(start_dt,end_dt)
+    model_xdata = get_date_strings(ilist)
+    data = list(cat_array[np.nonzero(cat_array['category'] == cat)[0]][list(str(i) for i in ilist)])
+    model_ydata = get_y_data(data,ilist)
+    ydat = list(model_ydata)
+    
+    pdf = pd.DataFrame(ydat)
+    pdf.index = pd.to_datetime(model_xdata)
+    result = seasonal_decompose(pdf, model='multiplicative')
+    trend =  result.trend[0].tolist()
+    resid = result.resid[0].tolist()
+    seasonal = result.seasonal[0].tolist()
+    return {'data': [{'x': model_xdata, 'y': model_ydata, 'type': 'line', 'name': 'PageViews'},
+                     {'x': date_strings, 'y': trend, 'type': 'line', 'name': 'trend','color':'red'},],
+            'layout': {'title': 'Trend vs. Date'}}
 if __name__ == '__main__':
     app.run_server(debug=True)
